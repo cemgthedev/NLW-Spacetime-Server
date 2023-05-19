b646cc3349cd08066dea594ada5288aa6c600f1a
cemgthedev <cemg.the.dev@gmail.com>
2023-05-19 09:08:12 -0300

    implementando back end da aplicação

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,14 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
-const prisma = new PrismaClient()
+
+app.register(cors, {
+  origin: true,
+})
+
+app.register(memoriesRoutes)
 
 app
   .listen({
@@ -11,9 +17,3 @@ app
   .then(() => {
     console.log('HTTP server running on http://localhost:3333')
   })
-
-app.get('/', async () => {
-  const users = await prisma.user.findMany()
-
-  return users
-})
